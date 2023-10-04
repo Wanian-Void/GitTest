4f9559ed0f4b92a9262f21087ac5af33343df7bd
zhoujiahao <bilibiliwnvoid@sina.com>
2023-10-04 10:22:37 +0800

    第二次测试 commit

--- src/main/java/com/xxxx/OneTest.java
+++ src/main/java/com/xxxx/OneTest.java
@@ -3,6 +3,6 @@ package com.xxxx;
 public class OneTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
-        System.out.println("第一次修改测试");
+        System.out.println("测试");
     }
 }
