2b1c5412dd9f15ade021ae8d9327c28fc0e52749
zhoujiahao <bilibiliwnvoid@sina.com>
2023-10-04 10:30:19 +0800

    合并分支测试

--- src/main/java/com/xxxx/OneTest.java
+++ src/main/java/com/xxxx/OneTest.java
@@ -4,5 +4,6 @@ public class OneTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("测试");
+        System.out.println("合并分支测试");
     }
 }
