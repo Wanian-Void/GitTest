9e2c4f48a9213a10dbd787afcb388de27dcf91e2
zhoujiahao <bilibiliwnvoid@sina.com>
2023-10-04 10:33:48 +0800

    合并冲突测试

--- src/main/java/com/xxxx/OneTest.java
+++ src/main/java/com/xxxx/OneTest.java
@@ -5,5 +5,6 @@ public class OneTest {
         System.out.println("Hello Git!");
         System.out.println("测试");
         System.out.println("合并分支测试");
+        System.out.println("合并冲突测试");
     }
 }
